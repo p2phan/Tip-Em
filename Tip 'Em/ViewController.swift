467cf80851562d2e2ded808f1ff215348285486d
Peter Phan <p2phan@ucsd.edu>
2017-12-29 00:13:02 -0800

    functioanl tip app with split option

--- Tip 'Em/ViewController.swift
+++ Tip 'Em/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipField: UITextField!
+    @IBOutlet weak var splitField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.billField.becomeFirstResponder()
+   
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        
+        let split = Int(splitField.text!) ?? 1
+        let bill =  Double(billField.text!) ?? 0
+        let tipPercent = Int(tipField.text!) ?? 0
+        let tip = bill/Double(split) * Double(tipPercent)/100
+        let total = bill/Double(split) + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip )
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
